14cd39bf3b938609b5ee2d6715d208c6753eb6de
Jorge Saavedra <jorge@gilasw.com>
2020-04-27 17:49:47 -0700

    setting debug mode for background geolocation to false

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -29,7 +29,7 @@ export class HomePage {
       notificationTitle: 'Background tracking',
       notificationText: 'enabled',
       stopOnTerminate: false,
-      debug: true,
+      debug: false,
       url: this.base_server_url,
       postTemplate: {
         "telephoneADID": this.device_id.toString(),
